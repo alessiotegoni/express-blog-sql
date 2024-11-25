fe52d62a264dd417fe8c785b4e0b16871e9dc120
tega <alessiotegoni4@gmail.com>
2024-11-25 13:25:54 +0100

    [ADD] middlewares

--- main.js
+++ main.js
@@ -10,4 +10,16 @@ app.use(express.json());
 
 app.use("/posts", postRouter);
 
+app.use((_, res) => {
+  res.status(404).json({ error: "Risorsa non trovata" });
+});
+
+app.use((err, _, res) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    error: "Errore del server",
+    message: err.message || "Qualcosa è andato storto!",
+  });
+});
+
 app.listen(PORT, () => console.log(`✅ Server running on port: ${PORT}`));
